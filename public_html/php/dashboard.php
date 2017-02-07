790cd63228a2588b94a28e58110b817dd30a33b5
Maria <Maria@Maria-TOSH>
2017-02-08 00:00:00 +0100

    Funcionan login y registro, faltan sessions

--- public_html/php/dashboard.php
+++ public_html/php/dashboard.php
@@ -1,11 +1,11 @@
 <?php
-//session.start();
-//if(isset($SESSION['usuario'])){
+session_start();
+if(isset($_SESSION['usuario'])){
 ?>
 <html>
     <head></head>
     <body><p>OK dashboard</p></body>
 </html>
 <?php
-//}
+}
 ?>
